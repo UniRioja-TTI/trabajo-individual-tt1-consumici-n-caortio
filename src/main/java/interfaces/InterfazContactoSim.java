feef5c640e5f302e302e44eebe03437498f8c57f
caortio <caortio@unirioja.es>
2026-03-12 16:41:00 +0100

    Update InterfazContactoSim.java

--- src/main/java/interfaces/InterfazContactoSim.java
+++ src/main/java/interfaces/InterfazContactoSim.java
@@ -2,6 +2,7 @@ package interfaces;
 
 import java.util.List;
 
+ 
 public interface InterfazContactoSim {
 	public int solicitarSimulation(DatosSolicitud sol);
 	public DatosSimulation descargarDatos(int ticket);
