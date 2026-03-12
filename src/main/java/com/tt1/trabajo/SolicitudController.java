b99e21c8577494a5507a46a5b4b97a3e140f6a9a
caortio <caortio@unirioja.es>
2026-03-12 16:38:18 +0100

    Añadido todo lo del json y los ficheros de pr3

--- src/main/java/com/tt1/trabajo/SolicitudController.java
+++ src/main/java/com/tt1/trabajo/SolicitudController.java
@@ -1,20 +1,9 @@
 package com.tt1.trabajo;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
-import org.slf4j.Logger;
-
-import interfaces.InterfazContactoSim;
-import modelo.DatosSolicitud;
 
 @Controller
 public class SolicitudController {
